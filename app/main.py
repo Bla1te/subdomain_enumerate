9c3e20ac8c57f3d63e1b602f8f077f3cfd2552cd
BIaite <danya.sharukha@gmail.com>
2026-04-06 13:40:02 +0300

    -json, -output

--- app/main.py
+++ app/main.py
@@ -3,11 +3,14 @@ import asyncio
 from infrastructure.dns import get_ip
 from infrastructure.load_list import load_subdamins_list
 from usecases.func import get_subdomain_list
+import json
 
 
 async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('domain')
+    parser.add_argument('-json', action='store_true')
+    parser.add_argument('-output')
 
     enter = parser.parse_args()
 
@@ -19,8 +22,23 @@ async def main():
         get_ip=get_ip
     )
 
-    for i in res:
-        print(i.name, 'ip', i.ip)
+    
+
+    if enter.json:
+        res = [{'subdomain':i.name, 'ip': i.ip} for i in res]
+        print(json.dumps(res, indent=2))
+        if enter.output:
+            with open(enter.output, "w") as f:
+                json.dump(res, f, indent=2)
+    else:
+        for i in res:
+            print(i.name, i.ip)
+        if enter.output:
+            with open(enter.output, "w") as f:
+                for i in res:
+                    f.write(str(i.name)+' '+str(i.ip)+'\n')
+    
+    
 
     return res
 
